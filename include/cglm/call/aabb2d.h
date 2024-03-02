a02d20c848d16ddbd063f6f038bc12c7c1e5ebbb
Kharzette <6589537+Kharzette@users.noreply.github.com>
2024-03-01 19:58:11 -0600

    More constification.  This one is odd, it only showed up in the "swift" build.  Whatever that is.

--- include/cglm/call/aabb2d.h
+++ include/cglm/call/aabb2d.h
@@ -22,26 +22,26 @@ glmc_aabb2d_zero(vec2 aabb[2]);
 
 CGLM_EXPORT
 void
-glmc_aabb2d_copy(vec2 aabb[2], vec2 dest[2]);
+glmc_aabb2d_copy(const vec2 aabb[2], vec2 dest[2]);
 
 CGLM_EXPORT
 void
-glmc_aabb2d_transform(vec2 aabb[2], mat3 m, vec2 dest[2]);
+glmc_aabb2d_transform(const vec2 aabb[2], mat3 m, vec2 dest[2]);
 
 CGLM_EXPORT
 void
-glmc_aabb2d_merge(vec2 aabb1[2], vec2 aabb2[2], vec2 dest[2]);
+glmc_aabb2d_merge(const vec2 aabb1[2], vec2 aabb2[2], vec2 dest[2]);
 
 CGLM_EXPORT
 void
-glmc_aabb2d_crop(vec2 aabb[2], vec2 cropAabb[2], vec2 dest[2]);
+glmc_aabb2d_crop(const vec2 aabb[2], vec2 cropAabb[2], vec2 dest[2]);
 
 CGLM_EXPORT
 void
-glmc_aabb2d_crop_until(vec2 aabb[2],
-                     vec2 cropAabb[2],
-                     vec2 clampAabb[2],
-                     vec2 dest[2]);
+glmc_aabb2d_crop_until(const vec2 aabb[2],
+                       const vec2 cropAabb[2],
+                       const vec2 clampAabb[2],
+                       vec2 dest[2]);
 
 CGLM_EXPORT
 void
@@ -49,39 +49,39 @@ glmc_aabb2d_invalidate(vec2 aabb[2]);
 
 CGLM_EXPORT
 bool
-glmc_aabb2d_isvalid(vec2 aabb[2]);
+glmc_aabb2d_isvalid(const vec2 aabb[2]);
 
 CGLM_EXPORT
 float
-glmc_aabb2d_diag(vec2 aabb[2]);
+glmc_aabb2d_diag(const vec2 aabb[2]);
 
 CGLM_EXPORT
 void
-glmc_aabb2d_sizev(vec2 aabb[2], vec2 dest);
+glmc_aabb2d_sizev(const vec2 aabb[2], vec2 dest);
 
 CGLM_EXPORT
 float
-glmc_aabb2d_radius(vec2 aabb[2]);
+glmc_aabb2d_radius(const vec2 aabb[2]);
 
 CGLM_EXPORT
 void
-glmc_aabb2d_center(vec2 aabb[2], vec2 dest);
+glmc_aabb2d_center(const vec2 aabb[2], vec2 dest);
 
 CGLM_EXPORT
 bool
-glmc_aabb2d_aabb(vec2 aabb[2], vec2 other[2]);
+glmc_aabb2d_aabb(const vec2 aabb[2], const vec2 other[2]);
 
 CGLM_EXPORT
 bool
-glmc_aabb2d_point(vec2 aabb[2], vec2 point);
+glmc_aabb2d_point(const vec2 aabb[2], const vec2 point);
 
 CGLM_EXPORT
 bool
-glmc_aabb2d_contains(vec2 aabb[2], vec2 other[2]);
+glmc_aabb2d_contains(const vec2 aabb[2], const vec2 other[2]);
 
 CGLM_EXPORT
 bool
-glmc_aabb2d_circle(vec2 aabb[2], vec3 s);
+glmc_aabb2d_circle(const vec2 aabb[2], const vec3 s);
 
 #ifdef __cplusplus
 }
